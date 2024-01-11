a42d965ab91375cba61fd3ef9723b41ee3ecb3fb
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-01-11 23:49:05 +0530

    new

--- app/view/admin/feedsSelection/index.php
+++ app/view/admin/feedsSelection/index.php
@@ -24,10 +24,12 @@ $sidebar = new Sidebar("feedsSelection");
 
     <style>
         .main-grid {}
-        .divTile a{
+
+        .divTile a {
             text-decoration: none;
-            color: white;
+            color: black;
         }
+
         .divTileRow {
             width: 100%;
             height: 200px;
@@ -36,33 +38,40 @@ $sidebar = new Sidebar("feedsSelection");
             justify-content: left;
             align-items: center;
             margin-bottom: 30px;
+            margin-left: 25px;
         }
 
         .divTile {
             width: 25%;
             height: 175px;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+            box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+            border: 1px solid #d0d0d0;
             margin: 30px;
             display: flex;
             justify-content: center;
             align-items: center;
             border-radius: 10px;
             transition: all 0.3s ease-in-out;
-            color: white;
-            background-image: linear-gradient(45deg, #ff9b2d, #ff5755);
+            color: black;
+            /* background-image: linear-gradient(45deg, #ff9b2d, #ff5755); */
             opacity: 0.7;
+            /* background-image: url('http://127.0.0.1/aka-hub/public/assets/img/common/elections.jpg');
+            background-size: cover; */
         }
 
         .divTile:hover {
             width: 25%;
             height: 175px;
-            box-shadow: 0 0 40px rgba(0, 0, 0, 0.5);
+            transform: scale(1.025);
+            background-color: #f5f5f5;
+            background-color: #eeecec;
+            background-color: #bdd2f138;
             margin: 30px;
             display: flex;
             justify-content: center;
             align-items: center;
             cursor: pointer;
-            font-size: 20px;
+            font-size: 18px;
             text-decoration: none;
             color: white;
         }
