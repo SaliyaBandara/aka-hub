4b64aae14a7243fcf781bf077a2e705d8c3efe72
samudiperera09 <samudiperera125@gmail.com>
2024-01-24 01:02:58 +0530

    events fetching from DB created

--- app/controller/dashboard.php
+++ app/controller/dashboard.php
@@ -11,6 +11,7 @@ class Dashboard extends Controller
             'message' => 'Welcome to Aka Hub!'
         ];
 
+        $data["main_events"] = $this->model('readModel')->getAll("main_events");
         $this->view->render('dashboard/index', $data);
     }
 }
