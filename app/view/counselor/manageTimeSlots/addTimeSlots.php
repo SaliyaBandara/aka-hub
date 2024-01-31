0367cdd0599c01846e3df9b304f44e6afdccd82a
Virajith Dissanayaka <121111846+virajithdissanayaka@users.noreply.github.com>
2024-01-31 21:53:41 +0530

    create custom time slots popup done

--- app/view/counselor/manageTimeSlots/addTimeSlots.php
+++ app/view/counselor/manageTimeSlots/addTimeSlots.php
@@ -1,3 +1,4 @@
+
 <?php
 $HTMLHead = new HTMLHead($data['title']);
 // $header = new header();
@@ -42,8 +43,10 @@ $calendar = new Calendar();
                     <p class="p1">Please add your available time slots here</p>
                 </div>
                 <div class="custom-button-div">
-                    <a href="#" class="button-custom">Create a Custom time slot</a>
+                    <a href="#divone" class="button-custom">Create a Custom time slot</a>
                 </div>
+                
+               
                 <div class="wrapper">    
                     <div class="card card-not-added" >
                             <div class="content">
@@ -106,223 +109,347 @@ $calendar = new Calendar();
                             </div>   
                     </div>
                 </div>
-                <!-- <div>
-                    <a href="google.com" class="button-custom">Create</a>
-                </div> -->
+                <div class="new">
+                    <div class="overlay" id="divone">
+                        <div class="wrapper1 popup-form">
+                            <h2>Create Custom  Time  Slot</h2>
+                            <a href="" class="close">&times;</a>
+                            <div class="content">
+                                <div class="container">
+                                    <form class="form-1">
+                                        <label>Start Time</label>
+                                        <input type="time"> 
+                                        <label>End Time</label>
+                                        <input type="time"><br /><br/>  
+                                        <input type="submit" value="Create">
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div> 
         </div>
         <div class="right">
             
         </div>
-    </div>
-
-    <style>
-        .main-grid .left {
-            width: 75%;
-            height: 150vh;
-
-        }
-
-        .main-grid .right {
-            flex-grow: 1;
-            height: 150vh;
-        }
-
-        .threeCardDiv {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 1rem;
-            height: 175px;
-            width: 100%;
-            z-index: +5;
-            color: white;
-            padding: 25px;
-        }
-
-        .cardTotalUsers {
-            width: 27%;
-            height: 100%;
-            background-color: #2684FF;
-            border-radius: 10px;
-            box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
-            padding: 1rem;
-            justify-content: center;
-            align-items: center;
-            text-align: center;
-            display: flex;
-            margin-left: 50px;
-        }
-
-        .cardTotalUsers:hover {
-            box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
-        }
-
-        .cardActiveUsers {
-            width: 27%;
-            height: 100%;
-            background-color: #ff9b2d;
-            border-radius: 10px;
-            box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
-            padding: 1rem;
-            justify-content: center;
-            align-items: center;
-            display: flex;
-        }
-
-        .cardActiveUsers:hover {
-            box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
-        }
-
-        .cardNewUsers {
-            width: 27%;
-            height: 100%;
-            background-color: #2684FF;
-            border-radius: 10px;
-            box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
-            padding: 1rem;
-            justify-content: center;
-            align-items: center;
-            margin-right: 50px;
-            display: flex;
-        }
-
-        .cardNewUsers:hover {
-            box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
-        }
-
-        .divUsersContainor {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            text-align: center;
-        }
-
-    </style>
-
-    <style>
-        .main-div{
-            
-        }
-        .wrapper{
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            /* z-index: +5; */
-            margin-top: -200px;
-        }
-
-        .wrapper .card{
-           
-            width: 80%;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            padding: 20px;
-            position:relative;
-            margin-bottom: 20px;
-            border-radius: 20px 20px 20px 20px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
-        }
-
-        .card-not-added{
-            background: #eeecec;
-        }
-        .card-added{
-            background: #ff9b2d;
-        }
-        .card .content{
-            display: flex;
-            align-items: center;
-
-        }
-
-        .card .details {
-            margin-left: 80px;
-        }
-
-        .details span{
-            font-weight: 600;
-            font-size: 18px;
-        }
-        .details i{
-            margin-right: 30px;
-            margin-left: -50px;
-            font-size: 24px;
-        }
-        .card a{
-            text-decoration: none;
-            display: inline-block;
-            padding: 10px 20px;
-            border-radius: 25px;
-            color: #fff;
-            /* background: linear-gradient(to bottom, #bea2e7 0%, #86b7e7 100%); */
-        }
-
-        .date-range{
-            text-align: center;
-        }
-        .date-range input{
-            margin-right: 10px;
-            margin-left: 10px;
-            width: 20%;
-            padding: 8px;
-            border-radius: 8px;
-            border-style: groove;
-            background-color: #fff;
-        }
-        .button-add{
-            background: #2684FF;
-            width: 100px;
-            text-align: center;
-        }
-        .button-delete{
-            background: red;
-            width: 100px;
-            text-align: center;
-        }
-        .button-remove{
-            background: #2684FF;
-            width: 100px;
-            text-align: center;
-        }
-        .header-topic{
-            text-align: center;
-        }
-        .button-select{
-            text-decoration: none;
-            display: inline-block;
-            padding: 10px 20px;
-            border-radius: 25px;
-            color: #fff;
-            background: #2684FF;
-        }
-        .p1{
-            font-size: 20px;
-            font-weight: 700;
-        }
-        .p2{
-            font-size: 18px;
-            font-weight: 700;
-        }
-        .button-custom{
-            text-decoration: none;
-            display: inline-block;
-            padding: 10px 20px;
-            border-radius: 25px;
-            color: #fff;
-            background: #2684FF;
-            align-items: center;
-        }
-        .custom-button-div{
-            margin-bottom:40px;
-            text-align: center;
-        }
-    </style>
 
 </div>
+        
+    
+
+
+
+<style>
+    .main-grid .left {
+        width: 75%;
+        height: 150vh;
+
+    }
+
+    .main-grid .right {
+        flex-grow: 1;
+        height: 150vh;
+    }
+
+    .threeCardDiv {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+        height: 175px;
+        width: 100%;
+        z-index: +5;
+        color: white;
+        padding: 25px;
+    }
+
+    .cardTotalUsers {
+        width: 27%;
+        height: 100%;
+        background-color: #2684FF;
+        border-radius: 10px;
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
+        padding: 1rem;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        display: flex;
+        margin-left: 50px;
+    }
+
+    .cardTotalUsers:hover {
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
+    }
+
+    .cardActiveUsers {
+        width: 27%;
+        height: 100%;
+        background-color: #ff9b2d;
+        border-radius: 10px;
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
+        padding: 1rem;
+        justify-content: center;
+        align-items: center;
+        display: flex;
+    }
+
+    .cardActiveUsers:hover {
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
+    }
+
+    .cardNewUsers {
+        width: 27%;
+        height: 100%;
+        background-color: #2684FF;
+        border-radius: 10px;
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
+        padding: 1rem;
+        justify-content: center;
+        align-items: center;
+        margin-right: 50px;
+        display: flex;
+    }
+
+    .cardNewUsers:hover {
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
+    }
+
+    .divUsersContainor {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+
+</style>
+
+<style>
+    .main-div{
+        
+    }
+    .wrapper{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        /* z-index: +5; */
+        margin-top: -200px;
+    }
+
+    .wrapper .card{
+        
+        width: 80%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 20px;
+        position:relative;
+        margin-bottom: 20px;
+        border-radius: 20px 20px 20px 20px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+    }
+
+    .card-not-added{
+        background: #eeecec;
+    }
+    .card-added{
+        background: #ff9b2d;
+    }
+    .card .content{
+        display: flex;
+        align-items: center;
+
+    }
+
+    .card .details {
+        margin-left: 80px;
+    }
+
+    .details span{
+        font-weight: 600;
+        font-size: 18px;
+    }
+    .details i{
+        margin-right: 30px;
+        margin-left: -50px;
+        font-size: 24px;
+    }
+    .card a{
+        text-decoration: none;
+        display: inline-block;
+        padding: 10px 20px;
+        border-radius: 25px;
+        color: #fff;
+        /* background: linear-gradient(to bottom, #bea2e7 0%, #86b7e7 100%); */
+    }
+
+    .date-range{
+        text-align: center;
+    }
+    .date-range input{
+        margin-right: 10px;
+        margin-left: 10px;
+        width: 20%;
+        padding: 8px;
+        border-radius: 8px;
+        border-style: groove;
+        background-color: #fff;
+    }
+    .button-add{
+        background: #2684FF;
+        width: 100px;
+        text-align: center;
+    }
+    .button-delete{
+        background: red;
+        width: 100px;
+        text-align: center;
+    }
+    .button-remove{
+        background: #2684FF;
+        width: 100px;
+        text-align: center;
+    }
+    .header-topic{
+        text-align: center;
+    }
+    .button-select{
+        text-decoration: none;
+        display: inline-block;
+        padding: 10px 20px;
+        border-radius: 25px;
+        color: #fff;
+        background: #2684FF;
+    }
+    .p1{
+        font-size: 20px;
+        font-weight: 700;
+    }
+    .p2{
+        font-size: 18px;
+        font-weight: 700;
+    }
+    .button-custom{
+        text-decoration: none;
+        display: inline-block;
+        padding: 10px 20px;
+        border-radius: 25px;
+        color: #fff;
+        background: #2684FF;
+        align-items: center;
+    }
+    .custom-button-div{
+        margin-bottom:40px;
+        text-align: center;
+    }
+</style>
+
+<style>
+    .overlay{
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: rgba(0, 0, 0, 0.8);
+        transition: opacity 500ms;
+        visibility: hidden;
+        opacity: 0;
+    }
+    .overlay:target{
+        visibility: visible;
+        opacity: 1;
+    }
+    .wrapper1{
+        margin: 70px auto;
+        padding: 20px;
+        background: #e7e7e7;
+        border-radius: 5px;
+        width: 30%;
+        position: relative;
+        transition: all 5s ease-in-out;
+        margin-top: 300px;
+    }
+    .wrapper1 h2{
+        margin-top: 0;
+        color: #333;
+    }
+    .wrapper1 .close{
+        position: absolute;
+        top: 20px;
+        right: 30px;
+        transition: all 200ms;
+        font-weight: bold;
+        text-decoration: none;
+        color: #333;
+    }
+    .wrapper1 .content{
+        max-height: 30%;
+        overflow: auto;
+    }
+
+    /* form design */
+
+    .container{
+        border-radius: 10px;
+        background-color: #e7e7e7;
+        padding: 20px 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    form label{
+        text-transform: uppercase;
+        font-weight: 500;
+        letter-spacing: 3px;
+    }
+    .container input[type="text"]{
+        width: 100%;
+        padding: 12px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        box-sizing: border-box;
+        margin-top: 6px;
+        margin-bottom: 16px;
+        resize: vertical;
+    }
+    .container input[type="submit"]{
+        background-color: #2684FF;
+        color: #fff;
+        padding: 15px 50px;
+        border: none;
+        border-radius: 50px;
+        cursor: pointer;
+        font-size: 15px;
+        text-transform: uppercase;
+        letter-spacing: 3px;
+    }
+    .popup-form{
+        width: 40%;
+        padding: 8px;
+        border-radius: 8px;
+        border-style: groove;
+        background-color: #fff; 
+    }
+    .form1 {
+        align-items: center;
+    }
+
+    .form-1 {
+        text-align: center;
+    }
+
+    .form-1 input[type="time"],
+    .form-1 input[type="submit"] {
+        margin: 0 auto; /* Center horizontally */
+    }
+
+    .wrapper1 h2{
+        text-align: center;
+    }
+</style>
 
 <style>
     #sidebar-active {
@@ -340,3 +467,8 @@ $calendar = new Calendar();
         overflow: hidden;
     }
 </style>
+
+
+
+
+
