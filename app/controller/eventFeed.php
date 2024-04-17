a62617a78a4f30d28fb830709283dfdcb838fb99
samudiperera09 <samudiperera125@gmail.com>
2024-04-17 21:01:57 +0530

    club event feed done

---
+++ app/controller/eventFeed.php
@@ -0,0 +1,139 @@
+<?php
+class eventFeed extends Controller
+{
+
+    public function redirect($redirect = "")
+    {
+        header("Location: " . BASE_URL . "/eventFeed");
+        die();
+    }
+
+    public function index()
+    {
+        $this->requireLogin();
+        $data = [
+            'title' => 'Event Feed',
+            'message' => 'Welcome to Aka Hub!'
+        ];
+
+        // $post_id = 5;
+        if(!($_SESSION["club_rep"])){
+            $data["posts"] = $this->model('readModel')->getAllCounselorPosts(2);
+        }
+        else{
+            $data["posts"] = $this->model('readModel')->getCounselorPosts(2,$_SESSION["user_id"]);
+        }
+
+        // $data["user"] = $this->model('readModel')->getOne("user", $_SESSION["user_id"]);
+        // $data["comments"] = $this->model('readModel')->getPostComments($post_id);
+        // print_r($data["comments"]);
+
+        $this->view->render('clubRep/eventFeed/index', $data);
+    }
+
+    public function add_edit($id = 0)
+    {
+        $this->requireLogin();
+        if (!($_SESSION["club_rep"]))
+            $this->redirect();
+
+        $data = [
+            'title' => ($id == 0) ? 'Create Post' : 'Edit Post',
+            'message' => 'Welcome to Aka Hub!'
+        ];
+
+        $data["post_template"] = $this->model('readModel')->getEmptyCounselorPost();
+        $data["post"] = $data["post_template"]["empty"];
+        $data["post_template"] = $data["post_template"]["template"];
+
+        if (isset($_POST['add_edit'])) {
+            $values = $_POST["add_edit"];
+
+            $values["posted_by"] = $_SESSION["user_id"];
+            $values["post_image"] = $values["post_image"];
+            $values["type"] = '2';
+
+            $this->validate_template($values, $data["post_template"]);
+
+            if ($id == 0)
+                $result = $this->model('createModel')->insert_db("posts", $values, $data["post_template"]);
+            else
+                $result = $this->model('updateModel')->update_one("posts", $values, $data["post_template"], "id", $id, "i");
+
+            if ($result)
+                die(json_encode(array("status" => "200", "desc" => "Operation successful")));
+
+            die(json_encode(array("status" => "400", "desc" => "Something went wrong")));
+        }
+
+        $data["id"] = $id;
+        // $data["action"] = $action;
+
+        if ($id != 0) {
+            $data["post"] = $this->model('readModel')->getOne("posts", $id);
+            if (!$data["post"])
+                $this->redirect();
+        }
+
+        $this->view->render('clubRep/eventFeed/add_edit', $data);
+
+
+        // getEmptyCounselorPost
+    }
+
+    public function delete($id = 0)
+    {
+
+        $this->requireLogin();
+        if (!($_SESSION["club_rep"]))
+            $this->redirect();
+
+        if ($id == 0)
+            die(json_encode(array("status" => "400", "desc" => "Invalid post id")));
+
+        // $result = $this->model('deleteModel')->deleteOne("courses", $id);
+        $result = $this->model('deleteModel')->deleteOne("posts", $id);
+
+        if ($result)
+            die(json_encode(array("status" => "200", "desc" => "Operation successful")));
+
+        die(json_encode(array("status" => "400", "desc" => "Error while deleting post")));
+    }
+
+    public function like($id = 0)
+    {
+
+        $this->requireLogin();
+        // if ($_SESSION["user_role"] != 5)
+        //     $this->redirect();
+        $data["likes_template"] = $this->model('readModel')->getEmptyPostLikes();
+        $data["likes"] = $data["likes_template"]["empty"];
+        $data["likes_template"] = $data["likes_template"]["template"];
+        // print_r($data["likes_template"]);
+        // if ( isset($_POST['like'])) {
+
+            // $values = $_POST["like"];
+            // print_r($values);
+        
+        $values["user_id"] = $_SESSION["user_id"];
+        $values["post_id"] = $id;
+
+        $existingLike = $this->model('readModel')->getPostLikes($id,$_SESSION["user_id"]);
+        print_r($existingLike);
+
+        if ($existingLike) {
+            die(json_encode(array("status" => "400", "desc" => "You have already liked this post")));
+        }
+
+        $this->validate_template($values, $data["likes_template"]);
+
+        $result = $this->model('createModel')->insert_db("post_likes", $values, $data["likes_template"]);
+
+        if ($result)
+            die(json_encode(array("status" => "200", "desc" => "Liked")));
+
+        die(json_encode(array("status" => "400", "desc" => "Something went wrong")));
+        // }
+
+    }
+}
---
+++ app/view/clubRep/eventFeed/index.php
@@ -0,0 +1,351 @@
+<?php
+$HTMLHead = new HTMLHead($data['title']);
+// $header = new header();
+$sidebar = new Sidebar("eventFeed");
+?>
+
+
+<div id="sidebar-active" class="hideScrollbar">
+    <?php $welcomeSearch = new WelcomeSearch(); ?>
+    <div class="main-grid flex">
+        <div class="left">
+            <?php if ($_SESSION["club_rep"]) { ?>
+                <div class="mb-1 form-group">
+                    <a href="<?= BASE_URL ?>/eventFeed/add_edit/0/" class="btn btn-primary">
+                        <i class='bx bx-plus'></i> Add Post
+                    </a>
+                </div>
+            <?php } ?>
+            <h3 class="h3-CounselorFeed">Club Events Feed</h3>
+            <div class="divFeed">
+                <div class="divCounselorFeed">
+                    <div class="feedContainer">
+
+                        <?php 
+                            if (empty($data["posts"])) {
+                                echo "<div class='font-meidum text-muted'>You can publish a post using 'Add Post' button above!</div>";
+                            } else {
+                                foreach ($data["posts"] as $posts) {
+                                $img_src = USER_IMG_PATH . $posts["post_image"];
+                                $img_src_profile = USER_IMG_PATH . $posts["profile_img"];
+                        ?>
+                                    <div class="feedPost my-2">
+                                        <div class = "postDetails">
+                                            <div class = "detailsLeft">
+                                                <div class = "userImage">
+                                                    <img src="<?= $img_src_profile ?>" alt="">
+                                                </div>
+                                                <div class = "userDetails">
+                                                    <div class = "userName">
+                                                        <?= $posts["name"] ?>
+                                                    </div>
+                                                    <div class = "publishedDate">
+                                                        <?= date('d/m/y H:i', strtotime($posts["created_datetime"])) ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class = detailsRight>
+                                                <?php if ($_SESSION["club_rep"]) { ?>
+                                                    <div class="editDeleteButton">
+                                                        <a href="<?= BASE_URL ?>/eventFeed/add_edit/<?= $posts['id'] ?>" class="repDecline">
+                                                            <i class='bx bx-edit'></i>
+                                                        </a>
+                                                        <a class="repDecline delete-item" data-id="<?= $posts['id'] ?>">
+                                                            <i class='bx bx-trash text-danger'></i>
+                                                        </a>
+                                                    </div>
+                                                <?php } ?>
+                                            </div>
+                                        </div>
+                                        <div class = "postTitle my-1 font-medium"> <?= strtoupper($posts["title"]) ?></div> 
+                                        <img class="postImage" src="<?= $img_src ?>" alt="">
+                                        <!-- <img class="eventPost" src="<?= BASE_URL ?>/public/assets/user_uploads/ClubEventFeed/sample post 1.jpg" alt=""> -->
+                                        <p style="white-space: pre-line;">
+                                            <?= substr($posts["description"], 0, 500) . (strlen($posts["description"]) > 500 ? '...' : '') ?>
+                                        </p>
+                                        <div class = "postDetails">
+                                            <div class = "detailsLeft">
+                                                <div class="likeCommentButton">
+                                                    <!-- <a href="./counselorFeed/like/<?= $posts["id"] ?>"> -->
+                                                    <a class = "likePost" data-id = "<?= $posts["id"] ?>">
+                                                        <i class='bx bx-heart text-danger likeButton'></i>
+                                                    </a>
+                                                    <label class = "likeCountLabel">
+                                                        <?= ($posts['likesCount'] === null) ? '0 Likes' : $posts['likesCount'] . ' Likes' ?>
+                                                    </label>
+                                                    <a href = "#" class="commentsToggle">
+                                                        <i class='bx bx-message-rounded'></i>
+                                                    </a> 
+                                                    <label>
+                                                        <!-- <?= count($data["comments"]) ?>  -->
+                                                        2 Comments
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="commentsSection" style="display: none;">
+                                            <?php 
+                                                if (empty($data["comments"])) {
+                                                    echo "<div class='font-meidum text-muted'></div>";
+                                                } else {
+                                                    foreach ($data["comments"] as $comments) {
+                                            ?>
+                                                <p><?= $comments["comment"] ?></p>
+                                                <?php } ?>
+                                            <?php } ?>
+                                        </div>
+                                    </div>
+                            <?php } ?>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .postDetails{
+        /* border: 1px solid black; */
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+    }
+
+    .detailsLeft{
+        /* border: 1px solid red; */
+        width: 70%;
+        display: flex;
+        flex-direction:row;
+        padding: 1rem;
+    }
+
+    .userDetails{
+        display: flex;
+        flex-direction: column;
+        padding-left: 1rem;
+        /* border: 1px solid red; */
+        justify-content: center;
+    }
+
+    .detailsRight{ width: 30%;}
+
+    .editDeleteButton {
+        display: flex;
+        justify-content: right;
+        /* border: 1px solid red; */
+        padding: 1rem;
+        font-size: 20px;
+    }
+
+    
+    .editDeleteButton a{
+        text-decoration: none;
+        color: inherit;
+        margin-left: 5px;
+    }
+
+    .likeCommentButton{
+        display: flex;
+        justify-content: right;
+        /* border: 1px solid red; */
+        font-size: 20px;
+    }
+
+    .likeCommentButton a{
+        font-size: 28px;
+        margin-right: 5px;
+        text-decoration: none;
+        color: inherit;
+    }
+
+    .likeCommentButton label{
+        font-size: 12px;
+        margin-right: 25px;
+        padding-top: 10px;
+    }
+
+    .userName , .publishedDate{
+        font-size: 12px;
+    }
+
+    .feedContainor {
+        width: 100%;
+        height: 100%;
+        justify-content: center;
+    }
+
+    .divFeed{
+        /* border: 1px solid red; */
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+    }
+
+
+
+    .postImage {
+        width: 100%;
+        height: auto;
+        /* border: 1px solid blue; */
+    }
+
+    .feedPost {
+        text-align: center;
+        /* border: 1px solid black; */
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+        border-radius : 20px;
+        /* margin: 25px; */
+    }
+
+    .feedPost p {
+        text-align: justify;
+        padding: 20px 20px 0 20px;
+    }
+
+    .postTitle{
+        padding-top: 15px;
+        padding-left: 15px;
+        text-align: left;
+        /* font-weight: bold; */
+    }
+
+    .repDecline {
+        width: 15%;
+        height: 38px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-left: -10px;
+        margin-right: 13px;
+    }
+    .delete-item, .likePost{
+        cursor: pointer;
+    }
+
+    .h3-CounselorFeed {
+        text-align: center;
+    }
+
+    .divCounselorFeed {
+        width: 65%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+    }
+
+    .userImage{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        border: 2px solid #bdd2f1;
+        width: 3rem;
+        height: 3rem;
+        overflow: hidden;
+    }
+
+    .userImage img{
+        width: 6rem;
+        height: 6rem;
+        display: block;
+    }
+
+    .main-grid {
+    }
+
+    .main-grid .left {
+        width: 100%;
+        height: 3000px;
+    }
+</style>
+
+<?php $HTMLFooter = new HTMLFooter(); ?>
+<script>
+    let BASE_URL = "<?= BASE_URL ?>";
+</script>
+<!-- <script>
+    document.addEventListener("DOMContentLoaded", function() {
+        // Select all elements with the class 'commentsToggle'
+        const toggleButtons = document.querySelectorAll(".commentsToggle");
+
+        // Iterate over each toggle button
+        toggleButtons.forEach(function(toggleButton) {
+            // Add click event listener to each toggle button
+            toggleButton.addEventListener("click", function(event) {
+                event.preventDefault();
+                // Find the corresponding comments section based on the button's parent element
+                const commentsSection = toggleButton.closest(".postDetails").nextElementSibling;
+                // Toggle the display style of the comments section
+                commentsSection.style.display = commentsSection.style.display === "none" ? "block" : "none";
+            });
+        });
+    });
+
+</script> -->
+<script>
+    $(document).ready(function() {
+        $(document).on("click", ".delete-item", function() {
+            let id = $(this).attr("data-id");
+            let $this = $(this);
+
+            // confirm delete
+            if (!confirm("Are you sure you want to delete this post?"))
+                return;
+
+            $.ajax({
+                url: `${BASE_URL}/eventFeed/delete/${id}`,
+                type: 'post',
+                data: {
+                    delete: true
+                },
+                dataType: 'json',
+                success: function(response) {
+                    if (response['status'] == 200) {
+                        alertUser("success", response['desc'])
+                        $this.closest(".feedPost").remove();
+                    } else if (response['status'] == 403)
+                        alertUser("danger", response['desc'])
+                    else
+                        alertUser("warning", response['desc'])
+                },
+                error: function(ajaxContext) {
+                    alertUser("danger", "Something Went Wrong")
+                }
+            });
+        });
+
+        $(document).on("click", ".likePost", function() {
+            let id = $(this).attr("data-id");
+            let $this = $(this);
+
+            // // confirm delete
+            // if (!confirm("Are you sure you want to delete this post?"))
+            //     return;
+
+            $.ajax({
+                url: `${BASE_URL}/eventFeed/like/${id}`,
+                type: 'post',
+                data: {
+                    like: true
+                },
+                dataType: 'json',
+                success: function(response) {
+                    if (response['status'] == 200) {
+                        alertUser("danger", response['desc'])
+                        setTimeout(function() {
+                            location.reload();
+                        }, 500);
+                    } else if (response['status'] == 403)
+                        alertUser("danger", response['desc'])
+                    else
+                        alertUser("warning", response['desc'])
+                },
+                error: function(ajaxContext) {
+                    alertUser("danger", "You have already liked this post")
+                }
+            });
+        });
+    });
+</script>
