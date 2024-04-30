3f9397cfb991efa1870f5b63147e120b0a9c2228
samudiperera09 <samudiperera125@gmail.com>
2024-04-30 22:59:43 +0530

    Change Password Pop up done

--- app/view/student/studentProfile/index.php
+++ app/view/student/studentProfile/index.php
@@ -71,7 +71,7 @@ $sidebar = new Sidebar("studentProfile");
             </div>
             <div class="flex notificationSettings">
                 <div>
-                    <a href="<?= BASE_URL ?>/studentProfile/changePassword" class="btn btn-danger me-1">
+                    <a href="" class="btn btn-danger me-1" id = "changePasswordBtn">
                         Change Password
                     </a>
                 </div>
@@ -201,6 +201,43 @@ $sidebar = new Sidebar("studentProfile");
         </div>
     </div>
 
+    <div class="popupBackground"></div>
+
+    <div class="popup flex flex-column">
+        <div class="popupHeader">Change Password</div>
+        <div class="popupForm">
+            <form action="" method="post" class="form">
+                <div class="mb-1 form-group">
+                    <label for="name" class="form-label">Old Password</label>
+                    <div class="flex flex-row passwordBox">
+                        <input type="password" id="password" name="password" class="" value="">
+                        <div class="showIcon" style="display: inline;">
+                            <i class='bx bx-show feather-eye' style="cursor: pointer;"></i>
+                            <i class='bx bx-hide feather-eye-off' style="display: none; cursor: pointer;"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-1 form-group">
+                    <label for="name" class="form-label">New Password</label>
+                    <div class="flex flex-row passwordBox">
+                        <input type="password" id="passwordNew" name="passwordNew" class="" value="">
+                        <div class="showIcon" style="display: inline;">
+                            <i class='bx bx-show feather-eye-new' style="cursor: pointer;"></i>
+                            <i class='bx bx-hide feather-eye-off-new' style="display: none; cursor: pointer;"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-1-5 form-group">
+                    <button class="btn btn-primary" id="changePassword">Change Password</button>
+                </div>
+
+            </form>
+        </div>
+        <span class="closeButton">&times;</span>
+    </div>
+
+
     <style>
         .main-grid {}
 
@@ -307,10 +344,168 @@ $sidebar = new Sidebar("studentProfile");
             width: 20rem;
             background-color: #f5f5f5;
         }
+
+        .passwordBox {
+            display: block;
+            width: 100%;
+            height: calc(1.5em + 0.75rem + 2px);
+            padding: 0.375rem 0.75rem;
+            font-size: var(--rv-1-new);
+            font-weight: 400;
+            line-height: 1.5;
+            color: #495057;
+            background-color: #fff;
+            background-clip: padding-box;
+            border: 1px solid #ced4da;
+            border-radius: 0.25rem;
+            transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+        }
+
+        .passwordBox:focus-within {
+            color: #495057;
+            background-color: #fff;
+            border-color: #80bdff;
+            outline: 0;
+            box-shadow: 0 0 0 0.25rem rgba(0, 123, 255, 0.25);
+        }
+
+        .passwordBox input {
+            outline: none;
+            border: none;
+            width: 95%;
+        }
+
+        .passwordBox input:focus {
+            outline: none;
+            border: none;
+        }
+
+        .popup {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background-color: white;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+            z-index: 9999;
+            display: none;
+        }
+
+        .popupHeader {
+            font-size: 1.2rem;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+
+        .popupForm {
+            width: 500px;
+        }
+
+        .popupBackground {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent black background */
+            z-index: 9998;
+            display: none;
+        }
+
+        .closeButton {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            cursor: pointer;
+            font-size: 1.2rem;
+        }
+
+        
+
     </style>
 
 </div>
 <?php $HTMLFooter = new HTMLFooter(); ?>
 <script>
     let BASE_URL = "<?= BASE_URL ?>";
+</script>
+
+<script>
+    $(document).ready(function() {
+        $('#changePasswordBtn').click(function(event) {
+            event.preventDefault();
+
+            $('.popupBackground').fadeIn();
+            $('.popup').fadeIn();
+        });
+
+        $('.closeButton, .popupBackground').click(function() {
+            $('.popupBackground').fadeOut();
+            $('.popup').fadeOut();
+        });
+    });
+
+    $(document).on("click", ".feather-eye", function() {
+        $(this).hide();
+        $(this).siblings(".feather-eye-off").show();
+        $(this).parent().siblings("input").attr("type", "text");
+    });
+
+    $(document).on("click", ".feather-eye-off", function() {
+        $(this).hide();
+        $(this).siblings(".feather-eye").show();
+        $(this).parent().siblings("input").attr("type", "password");
+    });
+
+    $(document).on("click", ".feather-eye-new", function() {
+        $(this).hide();
+        $(this).siblings(".feather-eye-off-new").show();
+        $(this).parent().siblings("input").attr("type", "text");
+    });
+
+    $(document).on("click", ".feather-eye-off-new", function() {
+        $(this).hide();
+        $(this).siblings(".feather-eye-new").show();
+        $(this).parent().siblings("input").attr("type", "password");
+    });
+
+    $(document).on("click", "#changePassword", function(event) {
+
+        event.preventDefault();
+        var oldPassword = $('#password').val();
+        var newPassword = $('#passwordNew').val();
+
+        if (!confirm("Are you sure you want to change your password?"))
+            return;
+
+        $.ajax({
+            url: `${BASE_URL}/studentProfile/changePassword`,
+            type: 'post',
+            data: {
+                oldPassword: oldPassword,
+                newPassword: newPassword,
+                changePassword: true
+            },
+            dataType: 'json',
+            success: function(response) {
+                if (response['status'] == 200) {
+                    alertUser("success", response['desc'])
+                    setTimeout(function() {
+                        history.go(-1);
+                        window.close();
+                    }, 2000);
+
+                } else if (response['status'] == 403)
+                    alertUser("danger", response['desc'])
+                else
+                    alertUser("warning", response['desc'])
+            },
+            error: function(ajaxContext) {
+                alertUser("danger", "Something Went Wrong")
+            }
+    });
+});
+
 </script>
